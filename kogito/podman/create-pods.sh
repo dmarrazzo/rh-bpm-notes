df3776d951d5f914fe33f7594a3a125a95274156
Donato Marrazzo <donato.marrazzo@gmail.com>
2021-05-28 19:56:06 +0200

    kogito infra and services

---
+++ kogito/podman/create-pods.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+
+podman pod create --name kogito-infra \
+       -p 11222:11222 \
+       -p 2181:2181 \
+       -p 9092:9092 \
+       -p 29092:29092 \
+       -p 9090:9090 \
+       -p 3000:3000
+
+# infinispan
+podman run -d --pod kogito-infra --name infinispan \
+       -v ./infinispan/infinispan.xml:/opt/infinispan/server/conf/infinispan-demo.xml:Z \
+       quay.io/infinispan/server:11.0.4.Final \
+       /opt/infinispan/bin/server.sh -c infinispan-demo.xml
+
+# kafka zookeeper
+podman run -d --pod kogito-infra --name zookeeper --restart=always \
+       -e LOG_DIR=/tmp/logs \
+       docker.io/strimzi/kafka:0.20.1-kafka-2.6.0 \
+       sh -c "bin/zookeeper-server-start.sh config/zookeeper.properties"
+
+# kafka server
+podman run -d --pod kogito-infra --name kafka --restart=always \
+           -e KAFKA_BROKER_ID=0 \
+           -e KAFKA_ZOOKEEPER_CONNECT=localhost:2181 \
+           -e KAFKA_LISTENERS=INTERNAL://localhost:29092,EXTERNAL://localhost:9092 \
+           -e KAFKA_ADVERTISED_LISTENERS=INTERNAL://localhost:29092,EXTERNAL://localhost:9092 \
+           -e KAFKA_LISTENER_SECURITY_PROTOCOL_MAP=INTERNAL:PLAINTEXT,EXTERNAL:PLAINTEXT \
+           -e KAFKA_INTER_BROKER_LISTENER_NAME=INTERNAL \
+           -e KAFKA_AUTO_CREATE_TOPICS_ENABLE="true" \
+           -e KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR=1 \
+           -e LOG_DIR=/tmp/logs \
+           docker.io/strimzi/kafka:0.20.1-kafka-2.6.0 \
+           sh -c "bin/kafka-server-start.sh config/server.properties --override listeners=PLAINTEXT://localhost:9092 --override advertised.listeners=PLAINTEXT://localhost:9092 --override zookeeper.connect=localhost:2181"
+
+# kafka drop
+# podman run -d --pod kogito-infra --restart=always \
+#     -e KAFKA_BROKERCONNECT=localhost:9092 \
+#     -e JVM_OPTS="-Xms32M -Xmx128M" \
+#     -e SERVER_SERVLET_CONTEXTPATH="/" \
+#     obsidiandynamics/kafdrop:3.27.0
+
+# prometheus
+podman run -d --pod kogito-infra --name prometheus --restart=always \
+       -v ./prometheus/prometheus.yml:/etc/prometheus/prometheus.yml:Z \
+       docker.io/prom/prometheus:v2.8.0 \
+       --config.file=/etc/prometheus/prometheus.yml
+
+# grafana
+podman run -d --pod kogito-infra --name grafana --restart=always \
+       -v ./grafana/provisioning/:/etc/grafana/provisioning/:Z \
+       -e PROMETHEUS_URL=http://prometheus:9090 \
+       docker.io/grafana/grafana:6.6.1
+
+# kogito supporting services
+
+export KOGITO_VERSION=1.6.0
+
+# data-index pod
+podman pod create --name kogito-data-index \
+       -p 8180:8080
+
+# data-index
+       #-v ./target/protobuf:/home/kogito/data/protobufs/ \
+       #-e KOGITO_DATA_INDEX_PROPS=-Dkogito.protobuf.folder=/home/kogito/data/protobufs/ \
+podman run -d --pod kogito-data-index --name data-index --restart=always \
+       -e QUARKUS_INFINISPAN_CLIENT_SERVER_LIST=localhost:11222 \
+       -e KAFKA_BOOTSTRAP_SERVERS=localhost:29092 \
+       quay.io/kiegroup/kogito-data-index-infinispan:${KOGITO_VERSION}
+
+# management-console pod
+podman pod create --name kogito-management-console \
+       -p 8280:8080
+
+# management-console
+       #-v ./svg/:/home/kogito/data/svg/ \
+podman run -d --pod kogito-management-console --name management-console --restart=always \
+       -e KOGITO_DATAINDEX_HTTP_URL=http://localhost:8180 \
+       -e KOGITO_MANAGEMENT_CONSOLE_PROPS=-Dkogito.svg.folder.path=/home/kogito/data/svg \
+       quay.io/kiegroup/kogito-management-console:${KOGITO_VERSION} 
